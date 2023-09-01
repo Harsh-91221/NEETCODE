131cb8f3d6a01a73f6c1d86799c358dffe21485c
Harsh91221 <harshch91221@gmail.com>
2023-09-01 15:30:32 +0530

    CODE

--- ValidAnagram.cpp
+++ ValidAnagram.cpp
@@ -1,5 +1,6 @@
 // BRUTE FORCE
 // T.C -> O(log(n))
+// S.C -> O(1)
 class Solution
 {
 public:
@@ -16,6 +17,8 @@ public:
 };
 
 // OPTIMIZED SOLUTION
+// T.C -> O(n)
+// S>C -> O(n)
 class Solution
 {
 public:
@@ -37,6 +40,36 @@ public:
         for (int i = 0; i < h.size(); i++)
         {
             if (h[i] != 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+// ANOTHER OPTIMIZED SOLUTION USING MAP
+class Solution
+{
+public:
+    bool isAnagram(string s, string t)
+    {
+        unordered_map<char, int> mp1, mp2;
+        if (s.size() != t.size())
+        {
+            return false;
+        }
+        for (auto a : s)
+        {
+            mp1[a]++;
+        }
+        for (auto a : t)
+        {
+            mp2[a]++;
+        }
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (mp1[s[i]] != mp2[s[i]])
             {
                 return false;
             }
