45051c88581851d148165f4d7ec3f0dfd435fe78
Harsh91221 <harshch91221@gmail.com>
2023-09-22 12:30:08 +0530

    CODE

--- LongestRepeatingCharacter.cpp
+++ LongestRepeatingCharacter.cpp
@@ -8,30 +8,26 @@
     Time: O(n)
     Space: O(26)
 */
-
-int characterReplacement(string s, int k)
+class Solution
 {
-    int i = 0;             // Initialize a variable i to track the start of the current window.
-    int maxCount = 0;      // Initialize maxCount to track the maximum frequency of any character in the current window.
-    vector<int> ch(26, 0); // Create a vector ch to store the frequency of each character ('A' to 'Z').
-    int result = 0;        // Initialize result to store the length of the longest valid substring.
-    for (int j = 0; j < s.size(); j++)
+public:
+    int characterReplacement(string s, int k)
     {
-        // Increment the frequency of the current character s[j] in the vector ch.
-        ch[s[j] - 'A']++;
-        // Update maxCount to be the maximum frequency of any character in the current window.
-        maxCount = max(maxCount, ch[s[j] - 'A']);
-        // If the length of the current window minus maxCount is greater than k, we need to shrink the window.
-        if (j - i + 1 - maxCount > k)
+        int n = s.length();
+        int i = 0, j = 0, maxFreq = 0, maxLen = 0;
+        unordered_map<char, int> mp;
+        for (j = 0; j < n; j++)
         {
-            // Decrease the frequency of the character at the start of the window (s[i]).
-            ch[s[i] - 'A']--;
-            // Move the start of the window one position to the right.
-            i++;
+            mp[s[j]]++;
+            maxFreq = max(maxFreq, mp[s[j]]);
+            // SHRINK THE WINDOW
+            while ((j - i + 1) - maxFreq > k)
+            {
+                mp[s[i]]--;
+                i++;
+            }
+            maxLen = max(maxLen, j - i + 1);
         }
-        // Update the result with the maximum length seen so far.
-        result = max(result, j - i + 1);
+        return maxLen;
     }
-    // The result now contains the length of the longest valid substring.
-    return result;
-}
+};
