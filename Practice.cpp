26b9e2959169ada55a538a48dc1997549583caf5
Harsh91221 <harshch91221@gmail.com>
2023-10-02 00:41:08 +0530

    CODE

--- Practice.cpp
+++ Practice.cpp
@@ -1,25 +1,45 @@
-#include <iostream>
-#include <vector>
-#include <climits>
-using namespace std;
-long long maximumTripletValue(vector<int> &nums)
+//BRUTE FORCE
+class Solution
 {
-    long long max1 = 0;
-    long long max2 = INT_MIN;
-    long long ans = 0;
-    for (int i = 0; i < nums.size(); i++)
+public:
+    long long maximumTripletValue(vector<int> &nums)
     {
-        ans = max(ans, max2 * (nums[i]));
-        max2 = max(max2, max1 - nums[i]);
-        max1 = max(max1, (long long)(nums[i]));
+        long long n = nums.size();
+        if (n < 3)
+        {
+            return 0;
+        }
+        long long maxval = 0;
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = i + 1; j < n; ++j)
+            {
+                for (int k = j + 1; k < n; ++k)
+                {
+                    long long triplet_value = (long long)(nums[i] - nums[j]) * nums[k];
+                    maxval = (long long)max(maxval, triplet_value);
+                }
+            }
+        }
+        return maxval;
     }
-    return ans;
-}
+};
 
-int main()
+//OPTIMIZED
+class Solution
 {
-    vector<int> nums = {12, 6, 1, 2, 7};
-    long long result = maximumTripletValue(nums);
-    cout << "Maximum Triplet Value: " << result << endl;
-    return 0;
-}
+public:
+    long long maximumTripletValue(vector<int> &nums)
+    {
+        long long max1 = 0;
+        long long max2 = INT_MIN;
+        long long ans = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            ans = max(ans, max2 * nums[i]);
+            max2 = max(max2, max1 - nums[i]);
+            max1 = max(max1, (long long)nums[i]);
+        }
+        return ans;
+    }
+};
