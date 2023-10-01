9469f8c7d856b14d58332dffa52a3a4598ce0a7d
Harsh91221 <harshch91221@gmail.com>
2023-10-02 00:15:36 +0530

    CODE

--- Practice.cpp
+++ Practice.cpp
@@ -1,26 +1,25 @@
-class Solution {
-public:
-    int minOperations(vector<int>& nums, int k) {
-        int len=nums.size();
-        set<int> s;
-        for(int i=1;i<=k;i++)
-        {
-            s.insert(i);
-        }
-        int count=0;
-        for(int i=n-1;i>=0;i--)
-        {
-            int val=nums.get(i); 
-            if(h.contains(val))
-            {
-                h.remove(val);
-            }
-            count++;
-            if(h.size()==0)
-            {
-                break;
-            }
-        }
-        return count;
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+long long maximumTripletValue(vector<int> &nums)
+{
+    long long max1 = 0;
+    long long max2 = INT_MIN;
+    long long ans = 0;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        ans = max(ans, max2 * (nums[i]));
+        max2 = max(max2, max1 - nums[i]);
+        max1 = max(max1, (long long)(nums[i]));
     }
-};
+    return ans;
+}
+
+int main()
+{
+    vector<int> nums = {12, 6, 1, 2, 7};
+    long long result = maximumTripletValue(nums);
+    cout << "Maximum Triplet Value: " << result << endl;
+    return 0;
+}
