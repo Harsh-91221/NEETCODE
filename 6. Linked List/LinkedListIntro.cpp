b0609bcaaa4363a8ddef2671cf9d40d670940b6f
Harsh91221 <harshch91221@gmail.com>
2023-10-03 07:07:49 +0530

    CODE

--- 6. Linked List/LinkedListIntro.cpp
+++ 6. Linked List/LinkedListIntro.cpp
@@ -5,11 +5,6 @@ class Node
 public:
     int data;
     Node *next;
-    Node()
-    {
-        this->data = 0;
-        this->next = NULL;
-    }
     Node(int data)
     {
         this->data = data;
