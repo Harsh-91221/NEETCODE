cbb6e3a696119ce53339ee453236f888b3b578cb
Harsh91221 <harshch91221@gmail.com>
2023-10-06 10:21:00 +0530

    CODE

--- 6. Linked List/AddTwoNumbers.cpp
+++ 6. Linked List/AddTwoNumbers.cpp
@@ -39,21 +39,26 @@ public:
         }
         ListNode *head1 = l1;
         ListNode *head2 = l2;
-
+        // ADD BOTH LINKED LIST
         ListNode *anshead = NULL;
         ListNode *anstail = NULL;
         int carry = 0;
         while (head1 != NULL && head2 != NULL)
         {
+            // Find sum
             int sum = carry + head1->val + head2->val;
+            // Find digit to create node
             int digit = sum % 10;
+
+            // Calculate carry
             carry = sum / 10;
 
             // Create a new node for digit
             ListNode *newnode = new ListNode(digit);
-            // Attack the newnode to the anshead list
+            // Attach the newnode to the anshead list
             if (anshead == NULL)
             {
+                // First node insert kr rahe ho
                 anshead = newnode;
                 anstail = newnode;
             }
@@ -65,6 +70,7 @@ public:
             head1 = head1->next;
             head2 = head2->next;
         }
+        // Jab head1 ki length head2 se jyada hogi
         while (head1 != NULL)
         {
             int sum = carry + head1->val;
@@ -75,6 +81,7 @@ public:
             anstail = newnode;
             head1 = head1->next;
         }
+        // Jab head2 ki length head1 se jyada hogi
         while (head2 != NULL)
         {
             int sum = carry + head2->val;
@@ -85,6 +92,7 @@ public:
             anstail = newnode;
             head2 = head2->next;
         }
+        // Handle karo carry ko alag se
         while (carry != NULL)
         {
             int sum = carry;
