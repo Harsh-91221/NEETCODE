6b10c4a98a91c5276805e9a1f6be6cf22da680d2
Harsh91221 <harshch91221@gmail.com>
2023-10-06 22:38:11 +0530

    CODE

--- 6. Linked List/SortList.cpp
+++ 6. Linked List/SortList.cpp
@@ -23,3 +23,73 @@ public:
         return head;
     }
 };
+
+// OPTIMAL
+// T.C - O(nlog(n))
+// S.C - O(1)
+class Solution
+{
+public:
+    ListNode *midnode(ListNode *head)
+    {
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while (fast->next != NULL && fast->next->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+    ListNode *mergesort(ListNode *h1, ListNode *h2)
+    {
+        if (h1 == NULL)
+        {
+            return h2;
+        }
+        if (h2 == NULL)
+        {
+            return h1;
+        }
+        ListNode *ans = new ListNode(0);
+        ListNode *curr = ans;
+        while (h1 != NULL && h2 != NULL)
+        {
+            if (h1->val < h2->val)
+            {
+                curr->next = h1;
+                h1 = h1->next;
+            }
+            else
+            {
+                curr->next = h2;
+                h2 = h2->next;
+            }
+            curr = curr->next;
+        }
+        if (h1 != NULL)
+        {
+            curr->next = h1;
+            h1 = h1->next;
+        }
+        if (h2 != NULL)
+        {
+            curr->next = h2;
+            h2 = h2->next;
+        }
+        return ans->next;
+    }
+    ListNode *sortList(ListNode *head)
+    {
+        if (head == NULL || head->next == NULL)
+        {
+            return head;
+        }
+        ListNode *mid = midnode(head);
+        ListNode *newhead = mid->next;
+        mid->next = NULL;
+        ListNode *lefthalf = sortList(head);
+        ListNode *righthalf = sortList(newhead);
+        return mergesort(lefthalf, righthalf);
+    }
+};
