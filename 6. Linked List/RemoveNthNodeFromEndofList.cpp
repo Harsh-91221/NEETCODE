4e922f71d4383bf61bd6e0d270f98fc494f49c6a
Harsh91221 <harshch91221@gmail.com>
2023-10-06 15:44:09 +0530

    CODE

---
+++ 6. Linked List/RemoveNthNodeFromEndofList.cpp
@@ -0,0 +1,59 @@
+/*
+    Given head of a linked list, remove nth node from end of list
+    Ex. head = [1,2,3,4,5], n = 2 -> [1,2,3,5]
+
+    Create 2 pointers "n" apart, iterate until end, will be at nth
+
+    Time: O(n)
+    Space: O(1)
+*/
+// BRUTE FORCE
+class Solution
+{
+public:
+    ListNode *reverse(ListNode *head)
+    {
+        if (head == NULL || head->next == NULL)
+        {
+            return head;
+        }
+        ListNode *prev = NULL;
+        ListNode *curr = head;
+        while (curr != NULL)
+        {
+            ListNode *temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+    ListNode *removeNthFromEnd(ListNode *head, int n)
+    {
+        ListNode *reverseHead = reverse(head);
+        ListNode *curr = reverseHead;
+        int i = 1;
+        if (n == 1)
+        {
+            ListNode *newhead = curr->next;
+            curr->next = NULL;
+            delete reverseHead;
+            return reverse(newhead);
+        }
+        ListNode *prev = NULL;
+        while (i < n)
+        {
+            prev = curr;
+            curr = curr->next;
+            i++;
+        }
+        if (prev != NULL)
+        {
+            prev->next = curr->next;
+            curr->next = NULL;
+            delete curr;
+        }
+        ListNode *result = reverse(reverseHead);
+        return result;
+    }
+};
