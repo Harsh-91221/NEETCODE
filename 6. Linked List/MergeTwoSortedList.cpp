d64f6b441a5ab1d95fd2a54a9ded6c0cc75cbddd
Harsh91221 <harshch91221@gmail.com>
2023-10-08 21:47:29 +0530

    CODE

--- 6. Linked List/MergeTwoSortedList.cpp
+++ 6. Linked List/MergeTwoSortedList.cpp
@@ -1,3 +1,12 @@
+/*
+    Given heads of 2 sorted linked lists, merge into 1 sorted list
+    Ex. list1 = [1,2,4], list2 = [1,3,4] -> [1,1,2,3,4,4]
+
+    Create curr pointer, iterate thru, choose next to be lower one
+
+    Time: O(m + n)
+    Space: O(1)
+*/
 class Solution
 {
 public:
