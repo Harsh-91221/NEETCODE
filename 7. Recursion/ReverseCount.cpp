252c63463aa720fa8d30969409f2230689c8ba3b
Harsh91221 <harshch91221@gmail.com>
2023-10-10 00:38:32 +0530

    CODE

---
+++ 7. Recursion/ReverseCount.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+void count(int n)
+{
+    if (n == 0)
+    {
+        return;
+    }
+    cout << n << " ";
+    count(n - 1);
+}
+int main()
+{
+    int n;
+    cin >> n;
+    count(n);
+}
