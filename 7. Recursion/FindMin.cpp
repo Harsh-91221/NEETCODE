2f0b14de4af5bc80fcd86c2c60c5233b4cbdb410
Harsh91221 <harshch91221@gmail.com>
2023-10-10 01:20:54 +0530

    CODE

---
+++ 7. Recursion/FindMin.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+void find(int a[], int i, int n, int &mini)
+{
+    if (i >= n)
+    {
+        return;
+    }
+    if (mini > a[i])
+    {
+        mini = a[i];
+    }
+    find(a, i + 1, n, mini);
+}
+int main()
+{
+    int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 99, -4};
+    int mini = INT_MAX;
+    find(a, 0, 10, mini);
+    cout << mini;
+}
