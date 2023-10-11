b4614eea2bf32238a600bd2fac96d9610153da69
Harsh91221 <harshch91221@gmail.com>
2023-10-12 00:02:34 +0530

    CODE

--- 7. Recursion/CoinChange.cpp
+++ 7. Recursion/CoinChange.cpp
@@ -36,3 +36,48 @@ public:
         return solve(coins, amount);
     }
 };
+
+// Optimal - DP
+class Solution
+{
+public:
+    int solve(vector<int> &coins, int amount, vector<int> &dp)
+    {
+        if (amount == 0)
+        {
+            return 0;
+        }
+        if (amount < 0)
+        {
+            return INT_MAX;
+        }
+        if (dp[amount] != -1)
+        {
+            return dp[amount];
+        }
+        int mini = INT_MAX;
+        for (int i = 0; i < coins.size(); i++)
+        {
+            int ans = solve(coins, amount - coins[i], dp);
+            if (ans != INT_MAX)
+            {
+                mini = min(mini, ans + 1);
+            }
+        }
+        return dp[amount] = mini;
+    }
+    int coinChange(vector<int> &coins, int amount)
+    {
+        int n = coins.size();
+        vector<int> dp(amount + 1, -1);
+        int result = solve(coins, amount, dp);
+        if (result == INT_MAX)
+        {
+            return -1;
+        }
+        else
+        {
+            return result;
+        }
+    }
+};
