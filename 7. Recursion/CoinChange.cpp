024e6c54d6abb50124ce42b4e8954b42f481d5ab
Harsh91221 <harshch91221@gmail.com>
2023-10-11 08:07:12 +0530

    CODE

---
+++ 7. Recursion/CoinChange.cpp
@@ -0,0 +1,37 @@
+// Brute force - Recursion
+class Solution
+{
+public:
+    int solve(vector<int> &coins, int amount)
+    {
+        if (amount == 0)
+        {
+            return 0;
+        }
+        if (amount < 0)
+        {
+            return -1;
+        }
+        int mini = INT_MAX;
+        for (int i = 0; i < coins.size(); i++)
+        {
+            int ans = solve(coins, amount - coins[i]);
+            if (ans != -1)
+            {
+                mini = min(mini, ans + 1);
+            }
+        }
+        if (mini == INT_MAX)
+        {
+            return -1;
+        }
+        else
+        {
+            return mini;
+        }
+    }
+    int coinChange(vector<int> &coins, int amount)
+    {
+        return solve(coins, amount);
+    }
+};
