66c928cc59a434873da496a76d22331969381d87
Harsh91221 <harshch91221@gmail.com>
2023-10-12 23:36:51 +0530

    CODE

--- Practice.cpp
+++ Practice.cpp
@@ -1,14 +1,64 @@
-int solve(vector<int> &nums, int i, int &maxi, vector<int> &dp)
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Function to find prime factors of a number
+vector<int> primeFactors(int num)
 {
-    if (i >= nums.size())
+    vector<int> factors;
+    for (int i = 2; i * i <= num; i++)
     {
-        return 0;
+        while (num % i == 0)
+        {
+            factors.push_back(i);
+            num /= i;
+        }
     }
-    if (dp[i] != -1)
+    if (num > 1)
     {
-        return dp[i];
+        factors.push_back(num);
     }
-    int include = nums[i] + solve(nums, i + 2, maxi, dp);
-    int exclude = solve(nums, i + 1, maxi, dp);
-    dp[i] = max(include, exclude);
+    return factors;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<vector<int>> primeCounts(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        int num;
+        cin >> num;
+        vector<int> factors = primeFactors(num);
+        for (int factor : factors)
+        {
+            primeCounts[i].push_back(factor);
+        }
+    }
+
+    // Count the occurrences of each prime factor
+    vector<int> primeFrequency(1001, 0); // Assuming maximum prime factor is 1000
+    for (int i = 0; i < n; i++)
+    {
+        for (int factor : primeCounts[i])
+        {
+            primeFrequency[factor]++;
+        }
+    }
+
+    // Check if the count of each prime factor is divisible by n
+    for (int i = 2; i < primeFrequency.size(); i++)
+    {
+        if (primeFrequency[i] % n != 0)
+        {
+            cout << "NO" << endl;
+            return 0;
+        }
+    }
+
+    cout << "YES" << endl;
+    return 0;
 }
