cc58ab242e0befaba808899c48b2995d4fcf36c8
Harsh91221 <harshch91221@gmail.com>
2023-10-12 07:19:44 +0530

    CODE

---
+++ 7. Recursion/Maximumsumofnon-adjacentelements.cpp
@@ -0,0 +1,43 @@
+// Brute force - Recursion
+#include <bits/stdc++.h>
+void solve(vector<int> &nums, int i, int sum, int &maxi)
+{
+    if (i >= nums.size())
+    {
+        maxi = max(maxi, sum);
+        return;
+    }
+    solve(nums, i + 2, nums[i] + sum, maxi);
+    solve(nums, i + 1, sum, maxi);
+}
+int maximumNonAdjacentSum(vector<int> &nums)
+{
+    int n = nums.size();
+    int maxi = INT_MIN;
+    int sum = 0;
+    solve(nums, 0, sum, maxi);
+    return maxi;
+}
+
+// Optimal - DP
+int solve(vector<int> &nums, int i, int &maxi, vector<int> &dp)
+{
+    if (i >= nums.size())
+    {
+        return 0;
+    }
+    if (dp[i] != -1)
+    {
+        return dp[i];
+    }
+    int include = nums[i] + solve(nums, i + 2, maxi, dp);
+    int exclude = solve(nums, i + 1, maxi, dp);
+    dp[i] = max(include, exclude);
+}
+int maximumNonAdjacentSum(vector<int> &nums)
+{
+    int n = nums.size();
+    vector<int> dp(n + 1, -1);
+    int maxi = INT_MIN;
+    return solve(nums, 0, maxi, dp);
+}
--- Practice.cpp
+++ Practice.cpp
@@ -1,31 +1,14 @@
-int solve(int n, int x, int y, int z, vector<int> &dp)
+int solve(vector<int> &nums, int i, int &maxi, vector<int> &dp)
 {
-    if (n == 0)
+    if (i >= nums.size())
     {
         return 0;
     }
-    if (n < 0)
+    if (dp[i] != -1)
     {
-        return INT_MIN;
+        return dp[i];
     }
-    if (dp[n] != -1)
-    {
-        return dp[n];
-    }
-    int ans1 = solve(n - x, x, y, z, dp) + 1;
-    int ans2 = solve(n - y, x, y, z, dp) + 1;
-    int ans3 = solve(n - z, x, y, z, dp) + 1;
-    int ans = 0;
-    ans = max(ans1, max(ans2, ans3));
-    return dp[n] = ans;
-}
-int maximizeTheCuts(int n, int x, int y, int z)
-{
-    vector<int> dp(n + 1, -1);
-    int ans = solve(n, x, y, z, dp);
-    if (ans < 0)
-    {
-        return 0;
-    }
-    return ans;
+    int include = nums[i] + solve(nums, i + 2, maxi, dp);
+    int exclude = solve(nums, i + 1, maxi, dp);
+    dp[i] = max(include, exclude);
 }
