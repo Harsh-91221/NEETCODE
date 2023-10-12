f0f674b8749cc3fb5f534951ad2622d9233be790
Harsh91221 <harshch91221@gmail.com>
2023-10-12 07:33:14 +0530

    CODE

--- 7. Recursion/Maximumsumofnon-adjacentelements.cpp
+++ 7. Recursion/Maximumsumofnon-adjacentelements.cpp
@@ -7,7 +7,9 @@ void solve(vector<int> &nums, int i, int sum, int &maxi)
         maxi = max(maxi, sum);
         return;
     }
+    // Include - i+2 as we have to consider non adjacent elements for the sum
     solve(nums, i + 2, nums[i] + sum, maxi);
+    // Exlude i+1
     solve(nums, i + 1, sum, maxi);
 }
 int maximumNonAdjacentSum(vector<int> &nums)
