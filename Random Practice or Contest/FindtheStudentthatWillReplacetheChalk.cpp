83ad4718d8e4192b8aa64a11e18605ba05431b6d
Harsh91221 <harshch91221@gmail.com>
2023-10-13 22:22:31 +0530

    CODE

---
+++ Random Practice or Contest/FindtheStudentthatWillReplacetheChalk.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+public:
+    int chalkReplacer(vector<int> &chalk, int k)
+    {
+        int n = chalk.size();
+        long sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum += chalk[i];
+        }
+        // if k is larger than the total sum, it effectively starts over from student 0.
+        k = k % sum;
+        for (int i = 0; i < n; i++)
+        {
+            // there are not enough chalk pieces to solve the problem
+            if (k - chalk[i] < 0)
+            {
+                // student at index i will need to replace the chalk
+                return i;
+            }
+            else
+            {
+                // indicating that these pieces have been used
+                k -= chalk[i];
+            }
+        }
+        return n - 1;
+    }
+};
