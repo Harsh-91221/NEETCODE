8bee150c305ff7a80535612e5fd04db3511c376f
Harsh91221 <harshch91221@gmail.com>
2023-10-19 07:56:44 +0530

    CODE

--- Practice.cpp
+++ Practice.cpp
@@ -1,32 +1,26 @@
-class Solution
+void solve(string &str, int i, vector<vector<string>> &output)
 {
-public:
-    bool isIsomorphic(string s, string t)
+    // If 'i' is equal to or greater than the size of 'str', it means we have
+    // successfully generated a permutation, so we add it to the 'output'.
+    if (i >= str.size())
     {
-        unordered_map<char, char> mp1;
-        unordered_map<char, char> mp2;
-        if (s.size() != t.size())
-        {
-            return false;
-        }
-        for (int i = 0; i < s.size(); i++)
-        {
-            // Check if 's[i]' is already mapped to a character in 't' (mp1[s[i]).
-            if (mp1[s[i]] && mp1[s[i]] != t[i])
-            {
-                return false;
-            }
-            // Check if 't[i]' is already mapped to a character in 's' (mp2[t[i]).
-            if (mp2[t[i]] && mp2[t[i]] != s[i])
-            {
-                return false;
-            }
-            // Update the mappings in both maps:
-            // - Map 's[i]' to 't[i]' in mp1
-            // - Map 't[i]' to 's[i]' in mp2
-            mp1[s[i]] = t[i];
-            mp2[t[i]] = s[i];
-        }
-        return true;
+        output.push_back(str);
+        return;
     }
-};
+    // Iterate through elements in the range [i, str.size() - 1].
+    for (int j = i; j < str.size(); j++)
+    {
+        // Swap the elements at indices 'i' and 'j'.
+        swap(str[i], str[j]);
+        // Recursively call the 'solve' function for the next index (i + 1).
+        solve(str, i + 1, output);
+        // Restore the original order by swapping the elements back.
+        swap(str[i], str[j]);
+    }
+}
+vector<string> generatePermutations(string &str)
+{
+    vector<vector<string>> output;
+    solve(str, 0, output);
+    return output;
+}
