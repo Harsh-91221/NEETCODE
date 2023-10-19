32af0a9a9f5c8d84d8fdb2b30bce94334d6e885b
Harsh91221 <harshch91221@gmail.com>
2023-10-20 00:12:43 +0530

    CODE

--- 7. Recursion/PrintSubsequenceSumK.cpp
+++ 7. Recursion/PrintSubsequenceSumK.cpp
@@ -3,8 +3,10 @@
 using namespace std;
 void solve(vector<int> &a, int i, int k, int sum, vector<int> &output)
 {
+    // If we have processed all elements in 'a'
     if (i == a.size())
     {
+        // If the current subset's sum is equal to 'k', print it
         if (sum == k)
         {
             for (auto i : output)
@@ -15,18 +17,22 @@ void solve(vector<int> &a, int i, int k, int sum, vector<int> &output)
         }
         return;
     }
+    // Include the current element 'a[i]' in the subset
     output.push_back(a[i]);
     sum += a[i];
     solve(a, i + 1, k, sum, output);
 
+    // Exclude the current element 'a[i]' from the subset
     sum -= a[i];
     output.pop_back();
     solve(a, i + 1, k, sum, output);
 }
+
 int main()
 {
     vector<int> a = {1, 2, 1};
     vector<int> output;
     int k = 2;
     solve(a, 0, k, 0, output);
+    return 0;
 }
