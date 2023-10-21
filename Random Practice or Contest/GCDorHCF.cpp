bba2fc674f80b3acc3269d2f561ba564050b8c10
Harsh91221 <harshch91221@gmail.com>
2023-10-22 00:47:30 +0530

    CODE

---
+++ Random Practice or Contest/GCDorHCF.cpp
@@ -0,0 +1,21 @@
+// T.C - O(log n)
+// S.C - O(1)
+int calcGCD(int n, int m)
+{
+    while (n > 0 && m > 0)
+    {
+        if (n > m)
+        {
+            n = n % m;
+        }
+        else
+        {
+            m = m % n;
+        }
+    }
+    if (n == 0)
+    {
+        return m;
+    }
+    return n;
+}
