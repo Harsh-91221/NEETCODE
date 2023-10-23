1e0c07a2bfa50d6faf34220765ea16ade6e56f5f
Harsh91221 <harshch91221@gmail.com>
2023-10-23 11:31:52 +0530

    CODE

--- 7. Recursion/CombinationSumIV.cpp
+++ 7. Recursion/CombinationSumIV.cpp
@@ -3,16 +3,23 @@
 using namespace std;
 int solve(vector<int> &nums, int target, int i)
 {
+    // Base case: If the target is 0, we found a valid combination.
     if (target == 0)
     {
         return 1;
     }
+    // Base case: If we've exceeded the array size or the target is negative,
+    // we can't form a combination.
     if (i >= nums.size() || target < 0)
     {
         return 0;
     }
+    // Include the current element in the combination and recursively
+    // find combinations for the reduced target.
     int include = solve(nums, target - nums[i], 0);
+    // Exclude the current element and move on to the next one.
     int exclude = solve(nums, target, i + 1);
+    // Total combinations are the sum of included and excluded combinations.
     return include + exclude;
 }
 int combinationSum4(vector<int> &nums, int target)
