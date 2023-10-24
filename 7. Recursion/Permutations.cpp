a602631c4459eecaffecd8202c3b07d590210fd5
Harsh91221 <harshch91221@gmail.com>
2023-10-24 22:34:42 +0530

    CODE

--- 7. Recursion/Permutations.cpp
+++ 7. Recursion/Permutations.cpp
@@ -1,30 +1,44 @@
-class Solution
+#include <iostream>
+#include <vector>
+using namespace std;
+void solve(vector<int> &nums, int i, vector<vector<int>> &output)
 {
-public:
-    void solve(vector<int> &nums, int i, vector<vector<int>> &output)
+    // If 'i' is equal to or greater than the size of 'nums', it means we have
+    // successfully generated a permutation, so we add it to the 'output'.
+    if (i >= nums.size())
     {
-        // If 'i' is equal to or greater than the size of 'nums', it means we have
-        // successfully generated a permutation, so we add it to the 'output'.
-        if (i >= nums.size())
-        {
-            output.push_back(nums);
-            return;
-        }
-        // Iterate through elements in the range [i, nums.size() - 1].
-        for (int j = i; j < nums.size(); j++)
-        {
-            // Swap the elements at indices 'i' and 'j'.
-            swap(nums[i], nums[j]);
-            // Recursively call the 'solve' function for the next index (i + 1).
-            solve(nums, i + 1, output);
-            // Restore the original order by swapping the elements back.
-            swap(nums[i], nums[j]);
-        }
+        output.push_back(nums);
+        return;
     }
-    vector<vector<int>> permute(vector<int> &nums)
+    // Iterate through elements in the range [i, nums.size() - 1].
+    for (int j = i; j < nums.size(); j++)
     {
-        vector<vector<int>> output;
-        solve(nums, 0, output);
-        return output;
+        // Swap the elements at indices 'i' and 'j'.
+        swap(nums[i], nums[j]);
+        // Recursively call the 'solve' function for the next index (i + 1).
+        solve(nums, i + 1, output);
+        // Restore the original order by swapping the elements back.
+        swap(nums[i], nums[j]);
     }
-};
+}
+vector<vector<int>> permute(vector<int> &nums)
+{
+    vector<vector<int>> output;
+    solve(nums, 0, output);
+    return output;
+}
+
+int main()
+{
+    vector<int> input = {1, 2, 3};
+    vector<vector<int>> permutations = permute(input);
+    for (auto perm : permutations)
+    {
+        for (int num : perm)
+        {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
