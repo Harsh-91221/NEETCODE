a5c2341945d21ec725d2a4e4429a5c684a6e9241
Harsh91221 <harshch91221@gmail.com>
2023-10-24 16:20:35 +0530

    CODE

--- Random Practice or Contest/MajorityElement.cpp
+++ Random Practice or Contest/MajorityElement.cpp
@@ -3,24 +3,28 @@ class Solution
 public:
     int majorityElement(vector<int> &arr)
     {
+        // Initialize the candidate and count
         int candidate = arr[0];
         int count = 1;
         for (int i = 1; i < arr.size(); i++)
         {
+            // If the current element is the same as the candidate
             if (arr[i] == candidate)
             {
                 count++;
             }
-            else
+            else // If the current element is different from the candidate
             {
                 count--;
             }
+            // If count becomes zero, update the candidate
             if (count == 0)
             {
                 candidate = arr[i];
                 count = 1;
             }
         }
+        // Return the majority element
         return candidate;
     }
 };
