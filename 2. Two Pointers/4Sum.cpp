612b5a1294798b412e0501f10435a0191b67b3fc
Harsh91221 <harshch91221@gmail.com>
2023-10-26 01:11:50 +0530

    CODE

--- 2. Two Pointers/4Sum.cpp
+++ 2. Two Pointers/4Sum.cpp
@@ -68,3 +68,65 @@ public:
         return result;
     }
 };
+
+// Optimal
+// T.C - O(n^3)
+// S.C - O(1)
+class Solution
+{
+public:
+    vector<vector<int>> fourSum(vector<int> &nums, int target)
+    {
+        int n = nums.size();
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end()); // Sort the input array in ascending order.
+        for (int i = 0; i < n; i++)
+        {
+            if (i > 0 && nums[i] == nums[i - 1]) // Skip duplicate values for the first element.
+            {
+                continue;
+            }
+            for (int j = i + 1; j < n; j++)
+            {
+                if (j != (i + 1) && nums[j] == nums[j - 1]) // Skip duplicate values for the second element.
+                {
+                    continue;
+                }
+                int k = j + 1;
+                int l = n - 1;
+                while (k < l)
+                {
+                    long long sum = nums[i] + nums[j] + nums[k] + nums[l];
+
+                    if (sum == target)
+                    {
+                        vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+                        ans.push_back(temp); // Found a valid quadruplet.
+
+                        k++;
+                        l--;
+
+                        while (k < l && nums[k] == nums[k - 1]) // Skip duplicate values for the third element.
+                        {
+                            k++;
+                        }
+
+                        while (k < l && nums[l] == nums[l + 1]) // Skip duplicate values for the fourth element.
+                        {
+                            l--;
+                        }
+                    }
+                    else if (sum < target)
+                    {
+                        k++;
+                    }
+                    else
+                    {
+                        l--;
+                    }
+                }
+            }
+        }
+        return ans; // Return the list of unique quadruplets that sum to the target.
+    }
+};
--- Practice.cpp
+++ Practice.cpp
@@ -3,29 +3,55 @@ class Solution
 public:
     vector<vector<int>> fourSum(vector<int> &nums, int target)
     {
+        int n = nums.size();
         vector<vector<int>> ans;
-        set<vector<int>> st;
-        for (int i = 0; i < nums.size(); i++)
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < n; i++)
         {
-            for (int j = i + 1; j < nums.size(); j++)
+            if (i > 0 && nums[i] == nums[i - 1])
             {
-                set<long long> s;
-                for (int k = j + 1; k < nums.size(); k++)
+                continue;
+            }
+            for (int j = i + 1; j < n; j++)
+            {
+                if (j != (i + 1) && nums[j] == nums[j - 1])
                 {
-                    long long sum = nums[i] + nums[j];
+                    continue;
+                }
+                int k = j + 1;
+                int l = n - 1;
+                while (k < l)
+                {
+                    long long sum = nums[i];
+                    sum += nums[j];
                     sum += nums[k];
-                    long long fourth = target - sum;
-                    if (s.find(fourth) != s.end())
+                    sum += nums[l];
+                    if (sum == target)
                     {
-                        vector<int> temp = {nums[i], nums[j], nums[k], (int)fourth};
-                        sort(temp.begin(), temp.end());
-                        st.insert(temp);
+                        vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+                        ans.push_back(temp);
+                        k++;
+                        l--;
+                        while (k < l && nums[k] == nums[k - 1])
+                        {
+                            k++;
+                        }
+                        while (k < l && nums[l] == nums[l + 1])
+                        {
+                            l--;
+                        }
                     }
-                    s.insert(nums[k]);
+                    else if (sum < target)
+                    {
+                        k++;
+                    }
+                    else
+                    {
+                        l--;
+                    }
                 }
             }
         }
-        vector<vector<int>> result(st.begin(), st.end());
-        return result;
+        return ans;
     }
 };
