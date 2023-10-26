6b10eba2592f5d82fe67eec65aee96cf6d3cf673
Harsh91221 <harshch91221@gmail.com>
2023-10-27 01:36:26 +0530

    CODE

--- Random Practice or Contest/ReversePairs.cpp
+++ Random Practice or Contest/ReversePairs.cpp
@@ -6,6 +6,7 @@ public:
         vector<int> ans;
         int left = low;
         int right = mid + 1;
+        // Merge two sorted subarrays into a single sorted array
         while (left <= mid && right <= high)
         {
             if (nums[left] <= nums[right])
@@ -19,17 +20,19 @@ public:
                 right++;
             }
         }
+        // Add remaining elements from the left subarray, if any
         while (left <= mid)
         {
             ans.push_back(nums[left]);
             left++;
         }
+        // Add remaining elements from the right subarray, if any
         while (right <= high)
         {
             ans.push_back(nums[right]);
             right++;
         }
-        // Yeh step dhyan rakhna hai
+        // Update the original array with the sorted values
         for (int i = low; i <= high; i++)
         {
             nums[i] = ans[i - low];
@@ -39,6 +42,7 @@ public:
     {
         int count = 0;
         int right = mid + 1;
+        // Count pairs with the left and right subarrays
         for (int i = low; i <= mid; i++)
         {
             while (right <= high && nums[i] > 2 * (long long)nums[right])
@@ -52,12 +56,12 @@ public:
     int mergesort(vector<int> &nums, int low, int high)
     {
         int count = 0;
-        ;
         if (low >= high)
         {
             return count;
         }
         int mid = (low + high) / 2;
+        // Recursively split, sort, and count pairs in subarrays
         count += mergesort(nums, low, mid);
         count += mergesort(nums, mid + 1, high);
         count += countpairs(nums, low, mid, high);
