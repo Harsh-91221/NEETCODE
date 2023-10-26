a18aec6cded99f55a7365251cc6517c3647de70a
Harsh91221 <harshch91221@gmail.com>
2023-10-27 00:57:36 +0530

    CODE

--- Practice.cpp
+++ Practice.cpp
@@ -1,10 +1,27 @@
-#include <vector>
-void mergeTwoSortedArraysWithoutExtraSpace(vector<long long> &a, vector<long long> &b)
+#include <map>
+vector<int> findMissingRepeatingNumbers(vector<int> a)
 {
-    for (int i = 0; i < b.size(); i++)
+    int repeating = -1;
+    int missing = -1;
+    map<int, int> mp;
+    for (auto i : a)
     {
-        a.push_back(b[i]);
+        mp[i]++;
     }
-    sort(a.begin(), a.end());
-    b.clear();
+    for (int i = 0; i < a.size(); i++)
+    {
+        if (mp[i] == 2)
+        {
+            repeating = i;
+        }
+        if (mp[i] == 0)
+        {
+            missing = i;
+        }
+        if (repeating != -1 && missing != -1)
+        {
+            break;
+        }
+    }
+    return {repeating, missing};
 }
