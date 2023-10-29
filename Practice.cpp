7f5f51a6c679fe5e2e6566a80e8114770a4ff8b4
Harsh91221 <harshch91221@gmail.com>
2023-10-29 18:35:31 +0530

    CODE

--- Practice.cpp
+++ Practice.cpp
@@ -1,49 +1,14 @@
 class Solution
 {
 public:
-    int M = 1e9 + 7;
-    int a = 0, e = 1, i = 2, o = 3, u = 4;
-    int solve(int ch, int n, vector<vector<int>> &dp)
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest)
     {
-        if (n == 0)
+        int T = minutesToTest / minutesToDie + 1;
+        int pigs = 0;
+        while (pow(T, pigs) < buckets)
         {
-            return 1;
+            pigs++;
         }
-        if (dp[ch][n] != -1)
-        {
-            return dp[ch][n] % M;
-        }
-        if (ch == a)
-        {
-            return dp[ch][n] = solve(e, n - 1, dp) % M;
-        }
-        else if (ch == e)
-        {
-            return dp[ch][n] = (solve(a, n - 1, dp) + solve(i, n - 1, dp)) % M;
-        }
-        else if (ch == i)
-        {
-            return dp[ch][n] = (solve(a, n - 1, dp) + solve(e, n - 1, dp) + solve(o, n - 1, dp) + solve(u, n - 1, dp)) % M;
-        }
-        else if (ch == o)
-        {
-            return dp[ch][n] = (solve(i, n - 1, dp) + solve(u, n - 1, dp)) % M;
-        }
-        else if (ch == u)
-        {
-            return dp[ch][n] = solve(a, n - 1, dp) % M;
-        }
-        return 0;
-    }
-    int countVowelPermutation(int n)
-    {
-        long result = 0;
-        vector<vector<long long>> dp(5, vector<long long>(n + 1, -1));
-        result = (result + solve(a, n - 1, dp)) % M;
-        result = (result + solve(e, n - 1, dp)) % M;
-        result = (result + solve(i, n - 1, dp)) % M;
-        result = (result + solve(o, n - 1, dp)) % M;
-        result = (result + solve(u, n - 1, dp)) % M;
-        return result;
+        return pigs;
     }
 };
