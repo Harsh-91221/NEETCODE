090057bd6b955782b3eb9a6b1b4b05f859fb7cd4
Harsh91221 <harshch91221@gmail.com>
2023-10-31 20:19:18 +0530

    CODE

--- Practice.cpp
+++ Practice.cpp
@@ -1,69 +1,16 @@
-class Solution
+int median(vector<vector<int>> &matrix, int m, int n)
 {
-public:
-    int findmaxIndex(vector<vector<int>> &mat, int n, int m, int col)
+    vector<int> arr;
+    for (int i = 0; i < matrix.size(); i++)
     {
-        int maxval = -1;
-        int index = -1;
-        // Iterate through the rows in the specified column.
-        for (int row = 0; row < n; row++)
+        for (int j = 0; j < matrix[0].size(); j++)
         {
-            // If the current element is greater than the current max value,
-            // update the max value and its index.
-            if (mat[row][col] > maxval)
-            {
-                maxval = mat[row][col];
-                index = row;
-            }
+            arr.push_back(matrix[i][j]);
         }
-        return index;
     }
-    vector<int> findPeakGrid(vector<vector<int>> &mat)
-    {
-        int n = mat.size();
-        int m = mat[0].size();
-        int s = 0;
-        int e = m - 1;
-        // Perform binary search on columns.
-        while (s <= e)
-        {
-            int mid = s + (e - s) / 2;
-            // Find the row index of the maximum value in the current column.
-            int maxrowIndex = findmaxIndex(mat, n, m, mid);
-            int left = 0;
-            int right = 0;
-            // Check if there is an element to the left of the current column.
-            if (mid - 1 >= 0)
-            {
-                left = mat[maxrowIndex][mid - 1];
-            }
-            else
-            {
-                left = -1;
-            }
-            // Check if there is an element to the right of the current column.
-            if (mid + 1 < m)
-            {
-                right = mat[maxrowIndex][mid + 1];
-            }
-            else
-            {
-                right = -1;
-            }
-            // Check if the current element is greater than its adjacent elements.
-            if (mat[maxrowIndex][mid] > left && mat[maxrowIndex][mid] > right)
-            {
-                return {maxrowIndex, mid};
-            }
-            else if (mat[maxrowIndex][mid] < left)
-            {
-                e = mid - 1;
-            }
-            else
-            {
-                s = mid + 1;
-            }
-        }
-        return {-1, -1};
-    }
-};
+    // Sort the array
+    sort(arr.begin(), arr.end());
+    // Find the median element
+    int mid = arr.size() / 2;
+    return arr[mid];
+}
