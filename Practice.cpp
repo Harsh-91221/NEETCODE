82d363d36a81b21242c263ac8ffa1d13b0ab0c20
Harsh91221 <harshch91221@gmail.com>
2023-11-04 17:52:59 +0530

    CODE

--- Practice.cpp
+++ Practice.cpp
@@ -1,17 +1,25 @@
 class Solution
 {
 public:
-    int getLastMoment(int n, vector<int> &left, vector<int> &right)
+    int beautySum(string s)
     {
-        int result = 0;
-        for (auto &a : left)
+        int ans = 0;
+        for (int i = 0; i < s.size(); i++)
         {
-            result = max(result, a);
+            unordered_map<char, int> mp;
+            for (int j = i; j < s.size(); j++)
+            {
+                int maxi = INT_MIN;
+                int mini = INT_MAX;
+                mp[s[j]]++;
+                for (auto a : mp)
+                {
+                    maxi = max(maxi, a.second);
+                    mini = min(mini, a.second);
+                }
+                ans += (maxi - mini);
+            }
         }
-        for (auto &a : right)
-        {
-            result = max(result, n - a);
-        }
-        return result;
+        return ans;
     }
 };
