4dcf7a692f7da583dc296f6b76768273313acd98
Harsh91221 <harshch91221@gmail.com>
2023-11-05 16:57:02 +0530

    CODE

--- Random Practice or Contest/FindtheWinnerofanArrayGame.cpp
+++ Random Practice or Contest/FindtheWinnerofanArrayGame.cpp
@@ -3,15 +3,17 @@ class Solution
 public:
     int getWinner(vector<int> &arr, int k)
     {
-        int maxi = *max_element(arr.begin(), arr.end());
+        int maxi = *max_element(arr.begin(), arr.end);
         int winner = arr[0];
         int streak = 0;
+        // If k is greater than or equal to the array size, return the maximum element.
         if (k >= arr.size())
         {
             return maxi;
         }
         for (int i = 1; i < arr.size(); i++)
         {
+            // Check if the current element is greater than the current winner.
             if (arr[i] > winner)
             {
                 winner = arr[i];
@@ -21,11 +23,13 @@ public:
             {
                 streak++;
             }
+            // If the streak reaches k or the winner is the maximum element, return the winner.
             if (streak == k || winner == maxi)
             {
                 return winner;
             }
         }
+        // Return the winner if the loop completes without reaching k or the maximum element.
         return winner;
     }
 };
