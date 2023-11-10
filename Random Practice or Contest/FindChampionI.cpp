78501654fff290c39c5d7b19e72e8fbde15fa182
Harsh91221 <harshch91221@gmail.com>
2023-11-10 22:41:55 +0530

    CODE

--- Random Practice or Contest/FindChampionI.cpp
+++ Random Practice or Contest/FindChampionI.cpp
@@ -3,29 +3,35 @@ class Solution
 public:
     int findChampion(vector<vector<int>> &grid)
     {
+        // Create an unordered map to store the count of wins for each team
         unordered_map<int, int> mp;
         for (int i = 0; i < grid.size(); i++)
         {
             for (int j = 0; j < grid.size(); j++)
             {
+                // Skip comparison with itself
                 if (i == j)
                 {
                     continue;
                 }
+                // If team i won against team j (grid[i][j] == 1), increment wins for team j
                 if (grid[i][j] == 1)
                 {
                     mp[j]++;
                 }
+                // If team i lost to team j (grid[i][j] == 0), increment wins for team i
                 else
                 {
                     mp[i]++;
                 }
             }
         }
+        // Iterate through each team to find the team with zero wins (champion)
         for (int team = 0; team < grid.size(); team++)
         {
             if (mp[team] == 0)
             {
+                // Return the index of the team with zero wins (champion)
                 return team;
             }
         }
