64ea2e567167769be6f75069a36c11cfffe7d5bc
Harsh91221 <harshch91221@gmail.com>
2023-11-15 12:06:48 +0530

    CODE

--- Practice.cpp
+++ Practice.cpp
@@ -1,43 +1,23 @@
 class Solution
 {
 public:
-    int countPalindromicSubsequence(string s)
+    int maximumElementAfterDecrementingAndRearranging(vector<int> &arr)
     {
-        unordered_map<int, pair<int, int>> mp;
-        for (char ch = 'a'; ch <= 'z'; ch++)
-        {
-            mp[ch] = {-1, -1};
-        }
-        int n = s.length();
+        int n = arr.size();
+        int maxi = INT_MIN;
+        sort(arr.begin(), arr.end());
         for (int i = 0; i < n; i++)
         {
-            char ch = s[i];
-            if (mp[ch].first == -1)
+            if (i == 0)
             {
-                mp[ch].first = i;
+                arr[0] = 1;
             }
-        }
-        for (int i = n - 1; i >= 0; i--)
-        {
-            char ch = s[i];
-            if (mp[ch].second == -1)
+            else if (abs(arr[i] - arr[i - 1]) > 1)
             {
-                mp[ch].second = i;
+                arr[i] = arr[i - 1] + 1;
             }
+            maxi = max(maxi, arr[i]);
         }
-        int ans = 0;
-        for (auto a : mp)
-        {
-            char ch = a.first;
-            int start = a.second.first;
-            int end = a.second.second;
-            unordered_set<int> st;
-            for (int i = start + 1; i < end; i++)
-            {
-                st.insert(s[i]);
-            }
-            ans += st.size();
-        }
-        return ans;
+        return maxi;
     }
 };
