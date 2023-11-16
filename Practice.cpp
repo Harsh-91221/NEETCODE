aa7243c4a46203abc76a9b138de1422c6b161565
Harsh91221 <harshch91221@gmail.com>
2023-11-16 09:55:44 +0530

    CODE

--- Practice.cpp
+++ Practice.cpp
@@ -1,37 +1,23 @@
-Node *reverse(Node *head)
+Node *uniqueSortedList(Node *head)
 {
-    if (head == NULL || head->next == NULL)
+    if (head == NULL)
     {
-        return head;
+        return NULL;
     }
-    Node *prev = NULL;
     Node *curr = head;
     while (curr != NULL)
     {
-        Node *temp = curr->next;
-        curr->next = prev;
-        prev = curr;
-        curr = temp;
+        if (curr->next != NULL && (curr->data == curr->next->data))
+        {
+            Node *next1 = curr->next->next;
+            Node *nodetodelete = curr->next;
+            delete (nodetodelete);
+            curr->next = next1;
+        }
+        else
+        {
+            curr = curr->next;
+        }
     }
-    return prev;
-}
-Node *removeKthNode(Node *head, int K)
-{
-    Node *dummy = new Node();
-    dummy->next = head;
-    Node *slow = dummy;
-    Node *fast = dummy;
-    int i = 1;
-    while (i <= K)
-    {
-        fast = fast->next;
-        i++;
-    }
-    while (fast->next != NULL)
-    {
-        slow = slow->next;
-        fast = fast->next;
-    }
-    slow->next = slow->next->next;
-    return dummy->next;
+    return head;
 }
