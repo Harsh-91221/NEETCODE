0dd540fdf4a75bacaa2ecd4172d4ec5a0b35360c
Harsh91221 <harshch91221@gmail.com>
2023-11-22 22:22:13 +0530

    CODE

--- Practice.cpp
+++ Practice.cpp
@@ -1,38 +1,52 @@
-void solve(vector<string> &ans, string digits, int index, vector<string> &mapping, string output)
+class Solution
 {
-    if (index >= digits.size())
+public:
+    int l, m, n;
+    vector<vector<int>> directions{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    bool find(vector<vector<char>> &board, int i, int j, string &word, int idx)
     {
-        ans.push_back(output);
-        return;
+        if (idx >= l)
+        {
+            return true;
+        }
+        if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word[idx])
+        {
+            return false;
+        }
+        char temp = board[i][j];
+        board[i][j] = '$';
+        for (auto &dir : directions)
+        {
+            int i_ = i + dir[0];
+            int j_ = j + dir[1];
+
+            if (find(board, i_, j_, word, idx + 1))
+            {
+                return true;
+            }
+        }
+        board[i][j] = temp;
+        return false;
     }
-    int digit = digits[index] - '0';
-    string value = mapping[digit];
-    for (int i = 0; i < value.size(); i++)
+    bool exist(vector<vector<char>> &board, string word)
     {
-        char ch = value[i];
-        output.push_back(ch);
-        solve(ans, digits, index + 1, mapping, output);
-        output.pop_back();
+        m = board.size();
+        n = board[0].size();
+        l = word.length();
+        if (m * n < l)
+        {
+            return false;
+        }
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (board[i][j] == word[0] && find(board, i, j, word, 0))
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
-}
-vector<string> letterCombinations(string digits)
-{
-    vector<string> ans;
-    string output = "";
-    if (digits.size() == 0)
-    {
-        return ans;
-    }
-    vector<string> mapping(10);
-    int index = 0;
-    mapping[2] = "abc";
-    mapping[3] = "def";
-    mapping[4] = "ghi";
-    mapping[5] = "jkl";
-    mapping[6] = "mno";
-    mapping[7] = "pqrs";
-    mapping[8] = "tuv";
-    mapping[9] = "wxyz";
-    solve(ans, digits, index, mapping, output);
-    return ans;
-}
+};
