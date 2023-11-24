a58e3ddbd5259c621eab0f0ff8393515e6c7d190
Harsh91221 <harshch91221@gmail.com>
2023-11-24 18:20:15 +0530

    CODE

--- Practice.cpp
+++ Practice.cpp
@@ -1,37 +1,15 @@
 class Solution
 {
 public:
-    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+    int maxCoins(vector<int> &piles)
     {
-        if (root == NULL)
+        int ans = 0;
+        int n = piles.size();
+        sort(piles.begin(), piles.end());
+        for (int i = n / 3; i < n; i += 2)
         {
-            return NULL;
+            ans += piles[i];
         }
-        if (root->val == p->val)
-        {
-            return p;
-        }
-        if (root->val == q->val)
-        {
-            return q;
-        }
-        TreeNode *leftans = lowestCommonAncestor(root->left, p, q);
-        TreeNode *rightans = lowestCommonAncestor(root->right, p, q);
-        if (leftans == NULL && rightans == NULL)
-        {
-            return NULL;
-        }
-        else if (leftans != NULL && rightans == NULL)
-        {
-            return leftans;
-        }
-        else if (leftans == NULL && rightans != NULL)
-        {
-            return rightans;
-        }
-        else
-        {
-            return root;
-        }
+        return ans;
     }
 };
