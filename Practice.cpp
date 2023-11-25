3fd996652891edf9d5cf18e18bd21f0e3c12719b
Harsh91221 <harshch91221@gmail.com>
2023-11-25 22:59:31 +0530

    CODE

--- Practice.cpp
+++ Practice.cpp
@@ -1,28 +1,22 @@
-class Solution
+int lowestCommonAncestor(TreeNode<int> *root, int x, int y)
 {
-public:
-    int maxDepth(TreeNode *root)
+    if (root == NULL)
     {
-        if (root == NULL)
-        {
-            return 0;
-        }
-        // Recursively find the maximum depth of the left subtree
-        int leftDepth = maxDepth(root->left);
-        // Recursively find the maximum depth of the right subtree
-        int rightDepth = maxDepth(root->right);
-        // Return the maximum depth among left and right subtrees, plus 1 for the current node
-        return max(leftDepth, rightDepth) + 1;
+        return (-1);
     }
-    int diameterOfBinaryTree(TreeNode *root)
+    if (root->data == x || root->data == y)
     {
-        if (root == NULL)
-        {
-            return NULL;
-        }
-        int ans1 = diameterOfBinaryTree(root->left);
-        int ans2 = diameterOfBinaryTree(root->right);
-        int ans3 = maxDepth(root->left) + maxDepth(root->right);
-        return max(ans1, max(ans2, ans3));
+        return (root->data);
     }
-};
+    int left = lowestCommonAncestor(root->left, x, y);
+    int right = lowestCommonAncestor(root->right, x, y);
+    if (left == -1)
+    {
+        return (right);
+    }
+    if (right == -1)
+    {
+        return (left);
+    }
+    return (root->data);
+}
