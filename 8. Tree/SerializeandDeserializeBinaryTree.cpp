6603a67293a18cc5ef080bbf2b756f5f862dc7fe
Harsh91221 <harshch91221@gmail.com>
2023-11-29 07:51:16 +0530

    CODE

--- 8. Tree/SerializeandDeserializeBinaryTree.cpp
+++ 8. Tree/SerializeandDeserializeBinaryTree.cpp
@@ -1,12 +1,3 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Codec
 {
 public:
@@ -84,7 +75,3 @@ public:
         return root;
     }
 };
-
-// Your Codec object will be instantiated and called as such:
-// Codec ser, deser;
-// TreeNode* ans = deser.deserialize(ser.serialize(root));
