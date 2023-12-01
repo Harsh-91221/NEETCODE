3fba8aaaf3caa0c3fa909cba2ef8487e8416755e
Harsh91221 <harshch91221@gmail.com>
2023-12-01 07:26:25 +0530

    CODE

--- 9. BST/DeleteNodeinaBST.cpp
+++ 9. BST/DeleteNodeinaBST.cpp
@@ -1,6 +1,7 @@
 class Solution
 {
 public:
+    // Function to find the maximum value in a subtree
     int maxVal(TreeNode *root)
     {
         if (root == NULL)
@@ -19,37 +20,45 @@ public:
         {
             return NULL;
         }
+        // If the current node's value matches the key
         if (root->val == key)
         {
+            // Case 1: Node has no children
             if (root->left == NULL && root->right == NULL)
             {
                 delete root;
                 return NULL;
             }
+            // Case 2: Node has only a left child
             else if (root->left != NULL && root->right == NULL)
             {
                 TreeNode *child = root->left;
                 delete root;
                 return child;
             }
+            // Case 3: Node has only a right child
             else if (root->left == NULL && root->right != NULL)
             {
                 TreeNode *child = root->right;
                 delete root;
                 return child;
             }
+            // Case 4: Node has both left and right children
             else
             {
+                // Find the inorder predecessor (maximum value in the left subtree)
                 int inorderPre = maxVal(root->left);
                 root->val = inorderPre;
                 root->left = deleteNode(root->left, inorderPre);
                 return root;
             }
         }
+        // If the key is smaller, go to the left subtree
         else if (root->val > key)
         {
             root->left = deleteNode(root->left, key);
         }
+        // If the key is larger, go to the right subtree
         else if (root->val < key)
         {
             root->right = deleteNode(root->right, key);
