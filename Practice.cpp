c014ca11557c5825c833d57784379e8b76051b8d
Harsh91221 <harshch91221@gmail.com>
2023-12-02 23:41:37 +0530

    CODE

--- Practice.cpp
+++ Practice.cpp
@@ -1,32 +1,35 @@
 class Solution
 {
 public:
-    bool solve(TreeNode *root, long long int lowerbound, long long int upperbound)
+    int countCharacters(vector<string> &words, string chars)
     {
-        if (root == NULL)
+        vector<int> charCount(26, 0);
+        for (auto ch : chars)
         {
-            return true;
+            charCount[ch - 'a']++;
         }
-        // Checking if the current node's value lies within the specified bounds
-        if (root->val > lowerbound && root->val < upperbound)
+        int result = 0;
+        for (auto word : words)
         {
-            // Recursively check the left subtree with updated upperbound as current node's value
-            bool leftans = solve(root->left, lowerbound, root->val);
-            // Recursively check the right subtree with updated lowerbound as current node's value
-            bool rightans = solve(root->right, root->val, upperbound);
-            // Return true only if both subtrees are valid BSTs
-            return leftans && rightans;
+            vector<int> wordCount(26, 0);
+            for (auto ch : word)
+            {
+                wordCount[ch - 'a']++;
+            }
+            bool ok = true;
+            for (int i = 0; i < 26; i++)
+            {
+                if (wordCount[i] > charCount[i])
+                {
+                    ok = false;
+                    break;
+                }
+            }
+            if (ok == true)
+            {
+                result += word.length();
+            }
         }
-        else
-        {
-            // If the current node's value violates the BST property, return false
-            return false;
-        }
-    }
-    bool isValidBST(TreeNode *root)
-    {
-        long long int lowerbound = -4294967296; // Lowerbound for INT_MIN
-        long long int upperbound = 4294967296;  // Upperbound for INT_MAX
-        return solve(root, lowerbound, upperbound);
+        return result;
     }
 };
