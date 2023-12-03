e5e9f95187262cbbbdb1d6f16de6f027dc2d8a5a
Harsh91221 <harshch91221@gmail.com>
2023-12-03 18:00:52 +0530

    CODE

--- 9. BST/TwoSumIV-InputisaBST.cpp
+++ 9. BST/TwoSumIV-InputisaBST.cpp
@@ -1,15 +1,16 @@
 class Solution
 {
 public:
+    // Perform inorder traversal to get sorted elements in 'inorder' vector
     void inorderTraverse(TreeNode *root, vector<int> &inorder)
     {
         if (root == NULL)
         {
             return;
         }
-        inorderTraverse(root->left, inorder);
-        inorder.push_back(root->val);
-        inorderTraverse(root->right, inorder);
+        inorderTraverse(root->left, inorder);  // Traverse left subtree
+        inorder.push_back(root->val);          // Store current node value in 'inorder'
+        inorderTraverse(root->right, inorder); // Traverse right subtree
     }
     bool findTarget(TreeNode *root, int k)
     {
@@ -17,6 +18,7 @@ public:
         inorderTraverse(root, inorder);
         int s = 0;
         int e = inorder.size() - 1;
+
         while (s < e)
         {
             int sum = inorder[s] + inorder[e];
