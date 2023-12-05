5a9fdcf3af525fbb3b0675558e38cad8a65bdb7a
Harsh91221 <harshch91221@gmail.com>
2023-12-05 21:41:10 +0530

    CODE

--- Practice.cpp
+++ Practice.cpp
@@ -1,14 +1,57 @@
-class Solution
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+bool isPossible(const vector<pair<int, int>> &segments, int maxDistance)
 {
-public:
-    int numberOfMatches(int n)
+    int left = 0, right = 0;
+    for (const auto &segment : segments)
     {
-        int ans = 0;
-        while (n > 1)
+        left = max(left - maxDistance, segment.first);
+        right = min(right + maxDistance, segment.second);
+        if (left > right)
         {
-            ans += n / 2;
-            n = (n + 1) / 2;
+            return false;
         }
-        return ans;
     }
-};
+    return true;
+}
+
+int findMinK(const vector<pair<int, int>> &segments)
+{
+    int low = 0, high = 1e9;
+    while (low < high)
+    {
+        int mid = (low + high) / 2;
+        if (isPossible(segments, mid))
+        {
+            high = mid;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return low;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<pair<int, int>> segments(n);
+        for (auto &segment : segments)
+        {
+            cin >> segment.first >> segment.second;
+        }
+        int minK = findMinK(segments);
+        cout << minK << endl;
+    }
+
+    return 0;
+}
