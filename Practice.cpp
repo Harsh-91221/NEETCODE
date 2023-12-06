efefdb0a2e9ead9538d8c167ebfe4494705e02d6
Harsh91221 <harshch91221@gmail.com>
2023-12-06 18:38:48 +0530

    CODE

--- Practice.cpp
+++ Practice.cpp
@@ -1,57 +1,13 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
-bool isPossible(const vector<pair<int, int>> &segments, int maxDistance)
+class Solution
 {
-    int left = 0, right = 0;
-    for (const auto &segment : segments)
+public:
+    int totalMoney(int n)
     {
-        left = max(left - maxDistance, segment.first);
-        right = min(right + maxDistance, segment.second);
-        if (left > right)
+        int ans = 0;
+        for (int i = 0; i < n; i++)
         {
-            return false;
+            ans += (i / 7 + 1) + (i % 7);
         }
+        return ans;
     }
-    return true;
-}
-
-int findMinK(const vector<pair<int, int>> &segments)
-{
-    int low = 0, high = 1e9;
-    while (low < high)
-    {
-        int mid = (low + high) / 2;
-        if (isPossible(segments, mid))
-        {
-            high = mid;
-        }
-        else
-        {
-            low = mid + 1;
-        }
-    }
-    return low;
-}
-
-int main()
-{
-    int t;
-    cin >> t;
-
-    while (t--)
-    {
-        int n;
-        cin >> n;
-        vector<pair<int, int>> segments(n);
-        for (auto &segment : segments)
-        {
-            cin >> segment.first >> segment.second;
-        }
-        int minK = findMinK(segments);
-        cout << minK << endl;
-    }
-
-    return 0;
-}
+};
