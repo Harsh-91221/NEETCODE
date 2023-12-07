95ac0b4ff86fc1f2f384dc4aab4390a0f4878cd5
Harsh91221 <harshch91221@gmail.com>
2023-12-07 23:05:06 +0530

    CODE

--- Practice.cpp
+++ Practice.cpp
@@ -5,15 +5,11 @@ public:
     {
         for (int i = num.size() - 1; i >= 0; i--)
         {
-            if (num[i] % 2 == 0)
+            if (num[i] % 2 != 0)
             {
-                num.pop_back();
-            }
-            else
-            {
-                break;
+                return num.substr(0, i + 1);
             }
         }
-        return num;
+        return "";
     }
 };
