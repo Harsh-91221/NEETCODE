6748ec1fc77383885098a3907f518c2bd035f871
Harsh91221 <harshch91221@gmail.com>
2023-12-11 21:25:20 +0530

    CODE

--- Practice.cpp
+++ Practice.cpp
@@ -1,53 +1,17 @@
 class Solution
 {
 public:
-    int calculate(string s)
+    int findSpecialInteger(vector<int> &arr)
     {
-        stack<int> st;
-        if (s.size() == 0)
+        int n = arr.size();
+        int quater = n / 4;
+        for (int i = 0; i < n - quater; i++)
         {
-            return 0;
-        }
-        int curr = 0;
-        char op = '+';
-        for (int i = 0; i < s.size(); i++)
-        {
-            if (isdigit(s[i]))
+            if (arr[i] == arr[i + quater])
             {
-                curr = curr * 10 + (s[i] - '0');
-            }
-            if (!isdigit(s[i]) && !isspace(s[i]) || i == s.size() - 1)
-            {
-                if (op == '+')
-                {
-                    st.push(curr);
-                }
-                else if (op == '-')
-                {
-                    st.push(-curr);
-                }
-                else if (op == '*')
-                {
-                    int temp = st.top();
-                    st.pop();
-                    st.push(temp * curr);
-                }
-                else if (op == '/')
-                {
-                    int temp = st.top();
-                    st.pop();
-                    st.push(temp / curr);
-                }
-                op = s[i];
-                curr = 0;
+                return arr[i];
             }
         }
-        int ans = 0;
-        while (!st.empty())
-        {
-            ans += st.top();
-            st.pop();
-        }
-        return ans;
+        return 0;
     }
 };
