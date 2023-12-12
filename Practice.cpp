401862a855c5e7204dd93377d0e6a7cf0f809ff6
Harsh91221 <harshch91221@gmail.com>
2023-12-12 07:33:22 +0530

    CODE

--- Practice.cpp
+++ Practice.cpp
@@ -1,17 +1,28 @@
 class Solution
 {
 public:
-    int maxProduct(vector<int> &nums)
+    int canCompleteCircuit(vector<int> &gas, vector<int> &cost)
     {
-        int maxi = INT_MIN;
-        for (int i = 0; i < nums.size(); i++)
+        int deficit = 0;
+        int start = 0;
+        int balance = 0;
+        for (int i = 0; i < gas.size(); i++)
         {
-            for (int j = i + 1; j < nums.size(); j++)
+            balance += gas[i] - cost[i];
+            if (balance < 0)
             {
-                int ans = (nums[i] - 1) * (nums[j] - 1);
-                maxi = max(maxi, ans);
+                deficit += balance;
+                start = i + 1;
+                balance = 0;
             }
         }
-        return maxi;
+        if (deficit + balance >= 0)
+        {
+            return start;
+        }
+        else
+        {
+            return -1;
+        }
     }
 };
