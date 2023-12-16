fc81367ac897aff330dd2339fa953e9373302020
Harsh91221 <harshch91221@gmail.com>
2023-12-16 21:54:32 +0530

    CODE

--- Practice.cpp
+++ Practice.cpp
@@ -1,25 +1,24 @@
 class Solution
 {
 public:
-    int strStr(string haystack, string needle)
+    int solve(vector<int> &nums, int i, vector<int> &dp)
     {
-        int n = haystack.size();
-        int m = needle.size();
-        for (int i = 0; i <= n - m; i++)
+        if (i >= nums.size())
         {
-            int j;
-            for (j = 0; j < m; j++)
-            {
-                if (haystack[i + j] != needle[j])
-                {
-                    break;
-                }
-            }
-            if (j == m)
-            {
-                return i;
-            }
+            return 0;
         }
-        return -1;
+        if (dp[i] != -1)
+        {
+            return dp[i];
+        }
+        int pick = nums[i] + solve(nums, i + 2, dp);
+        int not_pick = solve(nums, i + 1, dp);
+        return dp[i] = max(pick, not_pick);
+    }
+    int rob(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<int> dp(n + 1, -1);
+        return solve(nums, 0, dp);
     }
 };
