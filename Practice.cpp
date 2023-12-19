76a06c781abc4e986480dc3c5ee5dcb0202d680d
Harsh91221 <harshch91221@gmail.com>
2023-12-19 19:03:10 +0530

    CODE

--- Practice.cpp
+++ Practice.cpp
@@ -1,32 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution
 {
 public:
-    vector<vector<int>> directions{
-        {-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 0}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
-    vector<vector<int>> imageSmoother(vector<vector<int>> &img)
+    ListNode *insertionSortList(ListNode *head)
     {
-        int m = img.size();
-        int n = img[0].size();
-        vector<vector<int>> result(m, vector<int>(n, 0));
-        for (int i = 0; i < m; i++)
+        ListNode *curr = head;
+        ListNode *temp = new ListNode(0);
+        while (curr != NULL)
         {
-            for (int j = 0; j < n; j++)
+            ListNode *forward = curr->next;
+            ListNode *tempHead = temp;
+            while (tempHead->next != NULL && tempHead->next->val < curr->val)
             {
-                int sum = 0;
-                int count = 0;
-                for (auto a : directions)
-                {
-                    int i_ = i + a[0];
-                    int j_ = j + a[1];
-                    if (0 <= i_ && i_ < m && 0 <= j_ && j_ < n)
-                    {
-                        sum += img[i_][j_];
-                        count++;
-                    }
-                }
-                result[i][j] = sum / count;
+                tempHead = tempHead->next;
             }
+            curr->next = tempHead->next;
+            tempHead->next = curr;
+            curr = forward;
         }
-        return result;
+        return temp->next;
     }
 };
