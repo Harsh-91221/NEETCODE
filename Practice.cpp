94a883b41c5f2f48cf58155cbd13ecd964493807
Harsh91221 <harshch91221@gmail.com>
2023-12-19 17:09:00 +0530

    CODE

--- Practice.cpp
+++ Practice.cpp
@@ -1,22 +1,32 @@
 class Solution
 {
 public:
-    int findLengthOfLCIS(vector<int> &nums)
+    vector<vector<int>> directions{
+        {-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 0}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+    vector<vector<int>> imageSmoother(vector<vector<int>> &img)
     {
-        int maxi = 1;
-        int count = 1;
-        for (int i = 1; i < nums.size(); i++)
+        int m = img.size();
+        int n = img[0].size();
+        vector<vector<int>> result(m, vector<int>(n, 0));
+        for (int i = 0; i < m; i++)
         {
-            if (nums[i - 1] >= nums[i])
+            for (int j = 0; j < n; j++)
             {
-                count = 1;
+                int sum = 0;
+                int count = 0;
+                for (auto a : directions)
+                {
+                    int i_ = i + a[0];
+                    int j_ = j + a[1];
+                    if (0 <= i_ && i_ < m && 0 <= j_ && j_ < n)
+                    {
+                        sum += img[i_][j_];
+                        count++;
+                    }
+                }
+                result[i][j] = sum / count;
             }
-            else
-            {
-                count++;
-            }
-            maxi = max(maxi, count);
         }
-        return maxi;
+        return result;
     }
 };
