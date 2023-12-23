cd526daaa301a287fffa04201ae700910802116c
Harsh91221 <harshch91221@gmail.com>
2023-12-23 22:19:03 +0530

    CODE

--- Practice.cpp
+++ Practice.cpp
@@ -1,35 +1,39 @@
-class Solution
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+long long largestPerimeter(vector<int> &nums)
 {
-public:
-    bool isPathCrossing(string path)
+    long long i = 0;
+    int n = nums.size();
+    sort(nums.begin(), nums.end());
+    for (i = n - 1; i >= 0; i--)
     {
-        set<pair<int, int>> s;
-        int x = 0, y = 0;
-        s.insert({0, 0});
-        for (auto a : path)
+        long long sum = 0;
+        for (int j = i - 1; j >= 0; j--)
         {
-            if (a == 'N')
-            {
-                y++;
-            }
-            else if (a == 'S')
-            {
-                y--;
-            }
-            else if (a == 'E')
-            {
-                x++;
-            }
-            else
-            {
-                x--;
-            }
-            if (s.find({x, y}) != s.end())
-            {
-                return true;
-            }
-            s.insert({x, y});
+            sum += nums[j];
         }
-        return false;
+        if (nums[i] < sum)
+        {
+            break;
+        }
     }
-};
+    if (i < 2)
+    {
+        return -1;
+    }
+    long long ans = 0;
+    for (int k = 0; k <= i; k++)
+    {
+        ans += nums[k];
+    }
+    return ans;
+}
+int main()
+{
+    vector<int> input = {1, 12, 1, 2, 5, 50, 3};
+    long long result = largestPerimeter(input);
+    cout << "Largest perimeter: " << result << endl;
+    return 0;
+}
