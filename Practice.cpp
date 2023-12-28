1edbb10fd1b8f5c7309c4a28b200ef7af05c95d6
Harsh91221 <harshch91221@gmail.com>
2023-12-28 18:44:12 +0530

    CODE

--- Practice.cpp
+++ Practice.cpp
@@ -1,18 +1,43 @@
+int dp[101][27][101][101];
 class Solution
 {
 public:
-    int minCost(string colors, vector<int> &neededTime)
+    int n;
+    int solve(string &s, int i, int prevChar, int freq, int k)
     {
-        int ans = 0;
-        int n = colors.size();
-        for (int i = 1; i < n; i++)
+        if (k < 0)
         {
-            if (colors[i] == colors[i - 1])
-            {
-                ans += min(neededTime[i], neededTime[i - 1]);
-                neededTime[i] = max(neededTime[i], neededTime[i - 1]);
-            }
+            return INT_MAX;
         }
-        return ans;
+        if (i >= n)
+        {
+            return 0;
+        }
+        if (dp[i][prevChar][freq][k] != -1)
+        {
+            return dp[i][prevChar][freq][k];
+        }
+        int delete_i = solve(s, i + 1, prevChar, freq, k - 1);
+        int keep_i = 0;
+        if (s[i] - 'a' != prevChar)
+        {
+            keep_i = 1 + solve(s, i + 1, s[i] - 'a', 1, k);
+        }
+        else
+        {
+            int one_more_addition = 0;
+            if (freq == 1 || freq == 9 || freq == 99)
+            {
+                one_more_addition = 1;
+            }
+            keep_i = one_more_addition + solve(s, i + 1, prevChar, freq + 1, k);
+        }
+        return dp[i][prevChar][freq][k] = min(keep_i, delete_i);
+    }
+    int getLengthOfOptimalCompression(string s, int k)
+    {
+        n = s.size();
+        memset(dp, -1, sizeof(dp));
+        return solve(s, 0, 26, 0, k);
     }
 };
