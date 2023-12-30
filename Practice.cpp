a29f27c961cd1efef6fbb53fe1d33789e3dac853
Harsh91221 <harshch91221@gmail.com>
2023-12-30 19:07:56 +0530

    CODE

--- Practice.cpp
+++ Practice.cpp
@@ -1,32 +1,25 @@
 class Solution
 {
 public:
-    int minDifficulty(vector<int> &jobDifficulty, int d)
+    bool makeEqual(vector<string> &words)
     {
-        int n = jobDifficulty.size();
-        if (n < d)
+        unordered_map<char, int> mp;
+        int n = words.size();
+        for (auto st : words)
         {
-            return -1;
-        }
-        vector<vector<int>> dp(n, vector<int>(d + 1, -1));
-        for (int i = 0; i < n; i++)
-        {
-            dp[i][1] = *max_element(jobDifficulty.begin() + i, jobDifficulty.end());
-        }
-        for (int days = 2; days <= d; days++)
-        {
-            for (int i = 0; i <= n - days; i++)
+            for (auto a : st)
             {
-                int maxD = INT_MIN;
-                int result = INT_MAX;
-                for (int j = i; j <= n - days; j++)
-                {
-                    maxD = max(maxD, jobDifficulty[j]);
-                    result = min(result, maxD + dp[j + 1][days - 1]);
-                }
-                dp[i][days] = result;
+                mp[a]++;
             }
         }
-        return dp[0][d];
+        for (auto a : mp)
+        {
+            int freq = a.second;
+            if (freq % n != 0)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 };
