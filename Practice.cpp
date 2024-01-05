c146b569d6f0fd0cf1773ef497d3b009aca2ac5a
Harsh91221 <harshch91221@gmail.com>
2024-01-05 16:37:20 +0530

    CODE

--- Practice.cpp
+++ Practice.cpp
@@ -1,28 +1,20 @@
 class Solution
 {
 public:
-    int solve(vector<int> &nums, int i, int prev, vector<vector<int>> &dp)
+    string removeStars(string s)
     {
-        if (i >= nums.size())
+        string ans;
+        for (int i = 0; i < s.size(); i++)
         {
-            return 0;
+            if (s[i] == '*')
+            {
+                ans.pop_back();
+            }
+            else
+            {
+                ans.push_back(s[i]);
+            }
         }
-        if (dp[i][prev + 1] != -1)
-        {
-            return dp[i][prev + 1];
-        }
-        int include = 0;
-        if (prev == -1 || nums[i] > nums[prev])
-            include = 1 + solve(nums, i + 1, i, dp);
-        int exclude = solve(nums, i + 1, prev, dp);
-        int ans = max(include, exclude);
-        return dp[i][prev + 1] = ans;
-    }
-    int lengthOfLIS(vector<int> &nums)
-    {
-        int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-        int prev = -1;
-        return solve(nums, 0, prev, dp);
+        return ans;
     }
 };
