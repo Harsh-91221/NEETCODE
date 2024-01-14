bcbf53066e80d97c851f59d6cc89f615f4f9e612
Harsh91221 <harshch91221@gmail.com>
2024-01-14 15:00:31 +0530

    CODE

--- Practice.cpp
+++ Practice.cpp
@@ -1,32 +1,40 @@
 class Solution
 {
 public:
-    vector<string> splitWordsBySeparator(vector<string> &words, char separator)
+    vector<int> beautifulIndices(string s, string a, string b, int k)
     {
-        vector<string> ans;
-        for (int i = 0; i < words.size(); i++)
+        vector<int> ans;
+        vector<int> a_index;
+        vector<int> b_index;
+        int x = s.size();
+        int y = a.size();
+        int z = b.size();
+        for (int i = 0; i <= x - y; i++)
         {
-            string currentword;
-            for (int j = 0; j < words[i].size(); j++)
+            if (s.substr(i, y) == a)
             {
-                if (words[i][j] != separator)
-                {
-                    currentword += words[i][j];
-                }
-                else
-                {
-                    if (!currentword.empty())
-                    {
-                        ans.push_back(currentword);
-                        currentword = "";
-                    }
-                }
-            }
-            if (!currentword.empty())
-            {
-                ans.push_back(currentword);
+                a_index.push_back(i);
             }
         }
+        for (int i = 0; i <= x - z; i++)
+        {
+            if (s.substr(i, z) == b)
+            {
+                b_index.push_back(i);
+            }
+        }
+        for (int i = 0; i < a_index.size(); i++)
+        {
+            for (int j = 0; j < b_index.size(); j++)
+            {
+                if (abs(a_index[i] - b_index[j]) <= k)
+                {
+                    ans.push_back(a_index[i]);
+                    break;
+                }
+            }
+        }
+        sort(ans.begin(), ans.end());
         return ans;
     }
 };
