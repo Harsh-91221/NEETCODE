a7fe3ae19dc7399b75d1236c552959f41f1c6ade
Harsh91221 <harshch91221@gmail.com>
2024-01-15 19:15:56 +0530

    CODE

--- Practice.cpp
+++ Practice.cpp
@@ -1,38 +1,22 @@
 class Solution
 {
 public:
-    vector<vector<int>> findWinners(vector<vector<int>> &matches)
+    string defangIPaddr(string address)
     {
-        vector<int> mp(100001, -1);
-        for (auto a : matches)
+        string ans = "";
+        for (int i = 0; i < address.size(); i++)
         {
-            int Pwin = a[0];
-            int Plost = a[1];
-            if (mp[Pwin] == -1)
+            if (address[i] == '.')
             {
-                mp[Pwin] = 0;
-            }
-            if (mp[Plost] == -1)
-            {
-                mp[Plost] = 1;
+                ans += '[';
+                ans += '.';
+                ans += ']';
             }
             else
             {
-                mp[Plost]++;
+                ans += address[i];
             }
         }
-        vector<int> zeroLost, oneLost;
-        for (int i = 1; i < 100001; i++)
-        {
-            if (mp[i] == 0)
-            {
-                zeroLost.push_back(i);
-            }
-            else if (mp[i] == 1)
-            {
-                oneLost.push_back(i);
-            }
-        }
-        return {zeroLost, oneLost};
+        return ans;
     }
 };
