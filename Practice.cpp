ea76404ffc48c1688c7721ea479a4d7085fcc44f
Harsh91221 <harshch91221@gmail.com>
2024-01-20 17:22:33 +0530

    CODE

--- Practice.cpp
+++ Practice.cpp
@@ -1,31 +1,28 @@
-class Solution
+
+int solve(vector<vector<int>> &matrix, int row, int col, vector<vector<int>> &dp)
 {
-public:
-    int solve(vector<vector<int>> &matrix, int row, int col, vector<vector<int>> &dp)
+    if (row == 0 and col < matrix[0].size() and col >= 0)
     {
-        if (row == 0 and col < matrix[0].size() and col >= 0)
-        {
-            return matrix[row][col];
-        }
-        if (col >= matrix[0].size() or c < 0)
-        {
-            return INT_MAX;
-        }
-        if (dp[row][col] != INT_MAX)
-        {
-            return dp[row][col];
-        }
-        return dp[row][col] = matrix[row][col] + min(min(solve(matrix, row - 1, col + 1, dp), solve(matrix, row - 1, col, dp)), solve(matrix, row - 1, col - 1, dp));
+        return matrix[row][col];
     }
-    int minFallingPathSum(vector<vector<int>> &matrix)
+    if (col >= matrix[0].size() or c < 0)
     {
-        int m = matrix.size(), n = matrix[0].size();
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, INT_MAX));
-        int ans = INT_MAX;
-        for (int c = 0; c < n; c++)
-        {
-            ans = min(ans, solve(matrix, m - 1, c, dp));
-        }
-        return ans;
+        return INT_MAX;
     }
-};
+    if (dp[row][col] != INT_MAX)
+    {
+        return dp[row][col];
+    }
+    return dp[row][col] = matrix[row][col] + min(min(solve(matrix, row - 1, col + 1, dp), solve(matrix, row - 1, col, dp)), solve(matrix, row - 1, col - 1, dp));
+}
+int minFallingPathSum(vector<vector<int>> &matrix)
+{
+    int m = matrix.size(), n = matrix[0].size();
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, INT_MAX));
+    int ans = INT_MAX;
+    for (int c = 0; c < n; c++)
+    {
+        ans = min(ans, solve(matrix, m - 1, c, dp));
+    }
+    return ans;
+}
