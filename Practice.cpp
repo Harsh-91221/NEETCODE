fbc142a07aba30e8ed972c4a93c078131f55d733
Harsh91221 <harshch91221@gmail.com>
2024-01-24 13:37:08 +0530

    CODE

--- Practice.cpp
+++ Practice.cpp
@@ -1,19 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution
 {
 public:
-    int lengthOfLastWord(string s)
+    int ans = 0;
+    void solve(TreeNode *root, vector<int> &count)
     {
-        int count = 0;
-        int i = s.size() - 1;
-        while (i >= 0 && s[i] == ' ')
+        if (root == NULL)
         {
-            i--;
+            return;
         }
-        while (i >= 0 && s[i] != ' ')
+        count[root->val]++;
+        if (root->left == NULL && root->right == NULL)
         {
-            count++;
-            i--;
+            int oddfreq = 0;
+            for (int i = 1; i <= 9; i++)
+            {
+                if (count[i] % 2 != 0)
+                {
+                    oddfreq++;
+                }
+            }
+            if (oddfreq <= 1)
+            {
+                ans++;
+            }
         }
-        return count;
+        solve(root->left, count);
+        solve(root->right, count);
+        count[root->val]--;
+    }
+    int pseudoPalindromicPaths(TreeNode *root)
+    {
+        vector<int> count(10, 0);
+        solve(root, count);
+        return ans;
     }
 };
