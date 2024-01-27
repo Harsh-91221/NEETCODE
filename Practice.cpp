bac74467acabf4b116650422860966e481268cc2
Harsh91221 <harshch91221@gmail.com>
2024-01-27 16:07:08 +0530

    CODE

--- Practice.cpp
+++ Practice.cpp
@@ -1,32 +1,27 @@
 class Solution
 {
 public:
-    int MOD = 1e9 + 7;
-    int solve(int m, int n, int maxMove, int startRow, int startColumn, int moves, vector<vector<vector<int>>> &dp)
+    vector<int> shortestToChar(string s, char c)
     {
-        if (startRow >= m || startColumn >= n || startRow < 0 || startColumn < 0)
+        vector<int> ans;
+        for (int i = 0; i < s.size(); i++)
         {
-            return 1;
+            if (s[i] == c)
+            {
+                ans.push_back(i);
+            }
         }
-        if (moves == maxMove)
+        vector<int> result;
+        for (int i = 0; i < s.size(); i++)
         {
-            return 0;
+            int mini = INT_MAX;
+            for (int j = 0; j < ans.size(); j++)
+            {
+                int temp = abs(i - ans[j]);
+                mini = min(mini, temp);
+            }
+            result.push_back(mini);
         }
-        if (dp[moves][startRow][startColumn] != -1)
-        {
-            return dp[moves][startRow][startColumn];
-        }
-        long long ans = 0;
-        ans += solve(m, n, maxMove, startRow, startColumn - 1, moves + 1, dp) % MOD;
-        ans += solve(m, n, maxMove, startRow - 1, startColumn, moves + 1, dp) % MOD;
-        ans += solve(m, n, maxMove, startRow + 1, startColumn, moves + 1, dp) % MOD;
-        ans += solve(m, n, maxMove, startRow, startColumn + 1, moves + 1, dp) % MOD;
-        return dp[moves][startRow][startColumn] = ans % MOD;
-        ;
-    }
-    int findPaths(int m, int n, int maxMove, int startRow, int startColumn)
-    {
-        vector<vector<vector<int>>> dp(maxMove + 1, vector<vector<int>>(m + 1, vector<int>(n + 1, -1)));
-        return solve(m, n, maxMove, startRow, startColumn, 0, dp);
+        return result;
     }
 };
