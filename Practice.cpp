75c1fd5ef9b135fedfa2b7bb1e043ba4d346d729
Harsh91221 <harshch91221@gmail.com>
2024-02-02 15:05:07 +0530

    CODE

--- Practice.cpp
+++ Practice.cpp
@@ -1,26 +1,50 @@
 class Solution
 {
 public:
-    bool contain(string words, char x)
+    vector<int> sequentialDigits(int low, int high)
     {
-        for (int i = 0; i < words.size(); i++)
-        {
-            if (words[i] == x)
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-    vector<int> findWordsContaining(vector<string> &words, char x)
-    {
+        // queue<int> q;
+        // for (int i = 1; i <= 8; i++)
+        // {
+        //     q.push(i);
+        // }
+        // vector<int> ans;
+        // while (!q.empty())
+        // {
+        //     int temp = q.front();
+        //     q.pop();
+        //     if (temp >= low && temp <= high)
+        //     {
+        //         ans.push_back(temp);
+        //     }
+        //     int last_digit = temp % 10;
+        //     if (last_digit + 1 <= 9)
+        //     {
+        //         q.push(temp * 10 + (last_digit + 1));
+        //     }
+        // }
+        // return ans;
+        vector<int> pos = {12, 23, 34, 45, 56, 67, 78, 89,
+                           123, 234, 345, 456, 567, 678, 789,
+                           1234, 2345, 3456, 4567, 5678, 6789,
+                           12345, 23456, 34567, 45678, 56789,
+                           123456, 234567, 345678, 456789,
+                           1234567, 2345678, 3456789,
+                           12345678, 23456789,
+                           123456789};
+        int n = pos.size();
         vector<int> ans;
-        for (int i = 0; i < words.size(); i++)
+        for (int i = 0; i < n; i++)
         {
-            if (contain(words[i], x))
+            if (pos[i] < low)
             {
-                ans.push_back(i);
+                continue;
             }
+            if (pos[i] > high)
+            {
+                break;
+            }
+            ans.push_back(pos[i]);
         }
         return ans;
     }
