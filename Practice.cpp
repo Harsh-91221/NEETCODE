fb2c78c46a3527b374e040048efadc614a741b63
Harsh91221 <harshch91221@gmail.com>
2024-02-02 15:15:26 +0530

    CODE

--- Practice.cpp
+++ Practice.cpp
@@ -1,51 +1,28 @@
 class Solution
 {
 public:
-    vector<int> sequentialDigits(int low, int high)
+    int reverse(int n)
     {
-        // queue<int> q;
-        // for (int i = 1; i <= 8; i++)
-        // {
-        //     q.push(i);
-        // }
-        // vector<int> ans;
-        // while (!q.empty())
-        // {
-        //     int temp = q.front();
-        //     q.pop();
-        //     if (temp >= low && temp <= high)
-        //     {
-        //         ans.push_back(temp);
-        //     }
-        //     int last_digit = temp % 10;
-        //     if (last_digit + 1 <= 9)
-        //     {
-        //         q.push(temp * 10 + (last_digit + 1));
-        //     }
-        // }
-        // return ans;
-        vector<int> pos = {12, 23, 34, 45, 56, 67, 78, 89,
-                           123, 234, 345, 456, 567, 678, 789,
-                           1234, 2345, 3456, 4567, 5678, 6789,
-                           12345, 23456, 34567, 45678, 56789,
-                           123456, 234567, 345678, 456789,
-                           1234567, 2345678, 3456789,
-                           12345678, 23456789,
-                           123456789};
-        int n = pos.size();
-        vector<int> ans;
-        for (int i = 0; i < n; i++)
+        int s = 0;
+        while (n != 0)
         {
-            if (pos[i] < low)
-            {
-                continue;
-            }
-            if (pos[i] > high)
-            {
-                break;
-            }
-            ans.push_back(pos[i]);
+            int r = n % 10;
+            s = s * 10 + r;
+            n = n / 10;
         }
-        return ans;
+        return s;
+    }
+    int countDistinctIntegers(vector<int> &nums)
+    {
+        set<int> s;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            s.insert(nums[i]);
+        }
+        for (int i = 0; i < nums.size(); i++)
+        {
+            s.insert(reverse(nums[i]));
+        }
+        return s.size();
     }
 };
